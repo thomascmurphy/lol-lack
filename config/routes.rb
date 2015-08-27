643a8cbf1054e3e07360eaeb3c18ee903400977d
Thomas Murphy <thomas.c.murphy@gmail.com>
2015-08-27 09:18:05 -0600

    responsive styling and match count

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'about/index'
+
   get 'summoner/index'
   post 'summoner/search', to: 'summoner#search'
   get 'summoner/:summoner_name', to: 'summoner#show', as: 'summoner'
